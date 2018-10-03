165669722209ef3ea64a42206e5617d5a2f5fd26
vagrant <vagrant@yii2-code>
2018-10-03 22:27:17 +0300

    clean README.md

---
+++ frontend/controllers/LibraryController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anton
+ * Date: 19.09.18
+ * Time: 18:54
+ */
+
+namespace frontend\controllers;
+
+
+class LibraryController
+{
+
+}
---
+++ frontend/translations/app/ru-RU/app.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anton
+ * Date: 02.10.18
+ * Time: 0:36
+ */
