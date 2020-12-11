217270516e5b5587bc6641cfe8cc97d7a5a165ef
Anton Kalochelitis <fire.anton@gmail.com>
2020-12-11 23:42:19 +0200

    vagrant update install config

--- vagrant/provision/once-as-root.sh
+++ vagrant/provision/once-as-root.sh
@@ -36,8 +36,15 @@ info "Install additional software"
 apt-get install -y atop htop
 apt-get install -y mc nginx npm unzip screen
 apt-get install -y mysql-server-5.7 mysql-client-5.7
-apt-get install -y php7.4-bcmath php7.4-bz2 php7.4-cgi php7.4-cli php7.4-common php7.4-curl php7.4-dba php7.4-dev php7.4-enchant php7.4-fpm php7.4-gd php7.4-gmp php7.4-imap php7.4-interbase php7.4-intl php7.4-json php7.4-ldap php7.4-mbstring php7.4-mysql php7.4-odbc php7.4-opcache php7.4-pgsql php7.4-phpdbg php7.4-pspell php7.4-readline php7.4-recode php7.4-snmp php7.4-soap php7.4-sqlite3 php7.4-sybase php7.4-tidy php7.4-xml php7.4-xmlrpc php7.4-zip php7.4-xsl php7.4
+apt-get install -y php7.4 php7.4-fpm php7.4-mysql php7.4-bcmath php7.4-bz2 php7.4-cgi php7.4-cli php7.4-common php7.4-curl php7.4-dba
+apt-get install -y php7.4-gd php7.4-gmp php7.4-imap php7.4-interbase php7.4-intl php7.4-json php7.4-ldap php7.4-mbstring php7.4-enchant
+apt-get install -y php7.4-odbc php7.4-opcache php7.4-pgsql php7.4-phpdbg php7.4-pspell php7.4-readline php7.4-dev
+apt-get install -y php7.4-snmp php7.4-soap php7.4-sqlite3 php7.4-sybase php7.4-tidy php7.4-xml php7.4-xmlrpc php7.4-zip php7.4-xsl
+apt-get install -y php7.4-recode
 apt-get install -y php-memcached memcached php.xdebug
+
+info "Install Apache2-plugin-php"
+apt-get install -y libapache2-mod-php
 
 info "Configure MySQL"
 echo '[mysqld]
